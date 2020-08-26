5dc80a7c47cf65e3be4bd05418b79b0e08007034
konstantin_elagin <k.elagin@xsquare.ru>
2020-08-26 13:06:14 +0300

    eslint fixes

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const chalk = require(`chalk`);
 const http = require(`http`);
 const fs = require(`fs`).promises;
@@ -61,4 +63,4 @@ module.exports = {
         return console.info(chalk.green(`Ожидаю соединений на ${port}`));
       });
   }
-}
+};
